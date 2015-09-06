8ebe36cac8c194938121288c3dd8508f8adf3c6c
AmberWhiteSky <amberwhitesky@163.com>
2015-09-06 22:55:43 +0800

    share  message product about

--- 彩票练习项目副本(重构2)/彩票练习项目/AmSettingController.m
+++ 彩票练习项目副本(重构2)/彩票练习项目/AmSettingController.m
@@ -33,53 +33,66 @@
     
 }
 
+
+//第零组的数据
+-(void) setupZeroGroup{
+    SettingItem *pushNotice =[SettingArrowItem  itemWithIcon:@"MorePush" title:@"推送和提醒" destvcClass:[Test1Controller class]];
+    SettingItem *handShake =[SettingSwitchItem  itemWithIcon:@"handShake" title:@"摇一摇机选" ];
+    SettingItem *sound_Effect =[SettingSwitchItem  itemWithIcon:@"sound_Effect" title:@"声音效果"];
+    
+    GroupSetting *group = [[GroupSetting  alloc] init];
+    group.items =@[pushNotice,handShake,sound_Effect];
+    [self.data  addObject:group];
+    
+};
+
+
+//第一组的数据
+-(void) setupOneGroup{
+    //1组
+    SettingItem *MoreUpdate =[SettingItem  itemWithIcon:@"MoreUpdate" title:@"检查新版本"];
+    MoreUpdate.option =^{
+        NSLog(@"检查新版本");
+        [MBProgressHUD  showMessage:@"正在检查中"];
+#warning 发送网络请求去检查有没有最新版本
+        //几秒钟后消失
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            //移除hub
+            [MBProgressHUD  hideHUD  ];
+            
+            //提醒没有新版本
+            [MBProgressHUD  showError:@"没有新版本"];
+            
+        });
+    };
+    
+    SettingItem *help =[SettingArrowItem  itemWithIcon:@"MoreHelp" title:@"帮助" destvcClass:[Test1Controller class]];
+    
+    
+SettingItem *share =[SettingArrowItem  itemWithIcon:@"MoreHelp" title:@"分享" destvcClass:[Test1Controller class]];
+SettingItem *look=[SettingArrowItem  itemWithIcon:@"MoreHelp" title:@"查看消息" destvcClass:[Test1Controller class]];
+SettingItem *product =[SettingArrowItem  itemWithIcon:@"MoreHelp" title:@"产品推荐" destvcClass:[Test1Controller class]];
+SettingItem *about =[SettingArrowItem  itemWithIcon:@"MoreHelp" title:@"关于" destvcClass:[Test1Controller class]];
+
+    //        help.option =^{
+    //            NSLog(@"帮助");
+    //        };
+    GroupSetting *group = [[GroupSetting  alloc] init];
+    group.items =@[MoreUpdate,help,share,look,product,about];
+    [self.data  addObject:group];
+}
+
+
 - (NSMutableArray *)data {
     if (_data==nil) {
         _data =[NSMutableArray  array];
-        //0组
-        //        SettingItem *item00 = [SettingItem  itemWithIcon:@"MorePush" title:@"推送和提醒" vcClass:[AmPushNoticeController class]];
-        //         SettingItem *item01 = [SettingItem  itemWithIcon:@"handShake" title:@"推送和提醒22" vcClass:[AmPushNoticeController class]];
-        //1 组
-        //         SettingItem *item10 = [SettingItem  itemWithIcon:@"IDInfo" title:@"推送和提醒333" vcClass:[AmPushNoticeController class]];
         
+        //0组数据
+        [self  setupZeroGroup];
         
-        
-        //1组
-        SettingItem *pushNotice =[SettingArrowItem  itemWithIcon:@"MorePush" title:@"推送和提醒" destvcClass:[Test1Controller class]];
-        SettingItem *handShake =[SettingSwitchItem  itemWithIcon:@"handShake" title:@"摇一摇机选" ];
-        SettingItem *sound_Effect =[SettingSwitchItem  itemWithIcon:@"sound_Effect" title:@"声音效果"];
-        
-        GroupSetting *group0 = [[GroupSetting  alloc] init];
-        group0.items =@[pushNotice,handShake,sound_Effect];
-        [_data  addObject:group0];
-        
-        
-        //2组
-        SettingItem *MoreUpdate =[SettingItem  itemWithIcon:@"MoreUpdate" title:@"检查新版本"];
-        MoreUpdate.option =^{
-            NSLog(@"检查新版本");
-            [MBProgressHUD  showMessage:@"正在检查中"];
-#warning 发送网络请求去检查有没有最新版本
-            //几秒钟后消失
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-                //移除hub
-                [MBProgressHUD  hideHUD  ];
-                
-                //提醒没有新版本
-                [MBProgressHUD  showError:@"没有新版本"];
-                
-            });
-        };
-        
-        SettingItem *help =[SettingArrowItem  itemWithIcon:@"MoreHelp" title:@"帮助" destvcClass:[Test1Controller class]];
-        //        help.option =^{
-        //            NSLog(@"帮助");
-        //        };
-        GroupSetting *group2 = [[GroupSetting  alloc] init];
-        group2.items =@[MoreUpdate,help];
-        [_data  addObject:group2];
-        
-        
+        //1组数据
+        [self  setupOneGroup];
+    
     }
     return  _data;
 }
@@ -123,17 +136,6 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     
     // Configure the cell...
-    //创建cell
-    //    static NSString *ID =@"setting";
-    //    UITableViewCell *cell = [tableView  dequeueReusableCellWithIdentifier:ID];
-    //    if (cell==nil) {
-    //        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:ID];
-    //    }
-    //2.设置cell数据
-    //    SettingItem  *item =self.data [indexPath.section][indexPath.row];
-    //    cell.imageView.image = [UIImage  imageNamed:item.icon];
-    //    cell.textLabel.text =item.title;
-    
     //1.创建cell
     CustomCell *cell =[CustomCell  cellWithTableView:tableView];
     
@@ -144,22 +146,16 @@
     //3返回cell
     return cell;
     
-    //
-    //    cell.imageView.image =[UIImage imageNamed:item.icon];
-    //    cell.textLabel.text=item.title;
-    
     
     
     
     
 }
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    //取消选中这行
+    [tableView  deselectRowAtIndexPath:indexPath animated:YES];
     
-    //    SettingItem  *item = self.data[indexPath.section][indexPath.row];
-    //    UIViewController  *vc  =[[item.destvcClass  alloc]init];
-    //    [self.navigationController pushViewController:vc animated:YES];
-    
-    
+    //模型数据
     GroupSetting * group = self.data[indexPath.section];
     SettingItem *item = group.items[indexPath.row];
     
